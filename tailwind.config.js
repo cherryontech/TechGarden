3a1695bb51c4430b21557d8be80b5a6af4b525c0
Vanessa <zdperfectx@gmail.com>
2024-05-07 12:31:06 -0500

    background color added to body to apply to all components. adjust app css and apply styles using tailwindcss instead in app.js file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "cream": "#F5F4F2", // background color
+        "oasis-blue": "#B1DBD7" // navbar color
+      },
+    },
   },
   plugins: [],
 };
