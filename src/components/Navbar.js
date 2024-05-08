3c771f14e598ccb91ccfb87bf0468f002bac7dca
drojaa <119087348+drojaa@users.noreply.github.com>
2024-05-08 19:10:20 -0400

    Added login and signup button. Added logo.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,36 @@
 import { Link } from "react-router-dom";
 
 function Navbar() {
-
-  // temp color of bg-teal being used until pull request approved
   return (
-      <nav className="fixed top-0 left-0 right-0 z-10 flex justify-between items-center align-middle px-3 md:py-4 md:px-20 bg-oasis-blue">
-        <div>
+    <nav className=" top-0 left-0 right-0 z-10 flex justify-between items-center md:py-2 bg-oasis-blue px-4 md:px-20 relative">
+      <Link
+        to="/"
+        className="text-xl md:text-3xl font-semibold hover:text-gray-600 ml-[-4rem] mt-1"
+      >
+        Tech Garden
+      </Link>
+
+      <div className="flex-grow flex justify-start md:space-x-3 items-center ml-11 mt-1">
+        {[
+          ["Discover Tech Roles", "/tech-roles"],
+          ["Skill Evaluation", "/skill-eval"],
+          ["Knowledge Hub", "/knowledge-hub"],
+        ].map(([title, url], index) => (
           <Link
-            to="/"
-            className="text-xl md:text-2xl font-semibold hover:text-gray-600"
+            key={index}
+            to={url}
+            className="rounded-lg px-1 py-4 text-14 md:text-18 md:px-3 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900"
           >
-            Logo
+            {title}
           </Link>
-        </div>
-        <div className="flex md:space-x-4 items-center">
-          {[
-            ["Discover Tech Roles", "/tech-roles"],
-            ["Skill Evaluation", "/skill-eval"],
-            ["Knowledge Hub", "/knowledge-hub"],
-          ].map(([title, url, index]) => (
-            <Link
-              key={index}
-              to={url}
-              className="rounded-lg px-1 py-4 text-xs md:text-lg md:px-3 text-slate-700 font-medium hover:bg-slate-100 hover:text-slate-900"
-            >
-              {title}
-            </Link>
-          ))}
-        </div>
-      </nav>
+        ))}
+      </div>
+
+      <div className="absolute top-4 right-5 flex items-center space-x-2">
+        <button type="button" className="bg-white border border-gray-300 focus:outline-none font-small rounded-lg px-5 py-2 mr-1 dark:bg-oasis-blue dark:text-grey dark:border-gray-600">Log in</button>
+        <button type="button" className="bg-white border border-gray-300 focus:outline-none font-small rounded-lg px-4 py-2 dark:bg-gray-50 dark:text-grey dark:border-gray-600">Sign Up</button>
+      </div>
+    </nav>
   );
 }
 
