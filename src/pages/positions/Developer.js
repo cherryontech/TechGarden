b99c619d3beeee02c2ec32165891c2323d18ffa6
drojaa <119087348+drojaa@users.noreply.github.com>
2024-05-12 02:00:59 -0400

    Added files + folders for position

---
+++ src/pages/positions/Developer.js
@@ -0,0 +1,9 @@
+
+
+
+function Developer() {
+
+    <h1>Developer Page</h1>
+}
+
+export default Developer; 
---
+++ src/pages/positions/UXDesginer.js
@@ -0,0 +1,10 @@
+
+
+
+
+function UXDesigner() {
+
+    <h1>UX Designer Page</h1>
+}
+
+export default UXDesigner; 
